1e8ac14bb87fe8906dd27308af5ace40854928e3
Lasse Lybeck <lybeck.lasse@gmail.com>
2014-03-04 14:19:51 +0200

    Started work on tex

--- lap_mat.m
+++ lap_mat.m
@@ -6,8 +6,10 @@ f = f_eval(t);
 s = s(:);
 A = zeros(length(s), k);
 
+maxt = t(k);
+
 for ii = 1:k
-    A(:,ii) = exp(-s * t(ii)) / k;
+    A(:,ii) = maxt * exp(-s * t(ii)) / k;
 end
 A(:,1) = A(:,1) / 2;
 A(:,end) = A(:,end) / 2;
--- myinvlap.m
+++ myinvlap.m
@@ -1,7 +1,7 @@
 
 % number of points
-n = 100;
-k = 300;
+n = 300;
+k = 1000;
 
 % maximum value of s
 maxs = 100;
@@ -10,7 +10,7 @@ maxs = 100;
 maxt = 3;
 
 %noise level
-noise_lvl = 1e-8;
+noise_lvl = 1e-4;
 
 % data
 s = sort(maxs * rand(n,1));
@@ -21,7 +21,7 @@ t = linspace(0, maxt, k)';
 [A, f] = lap_mat(s, t);
 
 % measurement data with noise
-m = A * f + noise_lvl * randn(n, 1);
+m = mylaplace(s) + noise_lvl * randn(n, 1);
 
 [U, D, V] = svd(A);
 
@@ -29,15 +29,25 @@ trD = zeros(size(D));
 % number of singular values
 sings = min(size(D));
 
+errs = zeros(sings, 1);
+
 for ii = 1:sings
     
     trD(ii,ii) = D(ii, ii);
+    sng = trD(ii, ii);
     trpinvA = V * pinv(trD) * U';
     
     rec = trpinvA * m;
+    relerr = norm(rec(:)-f(:))/norm(f(:))*100;
+    errs(ii) = relerr;
     
-    fprintf('Using %d singular values.\n', ii)
+    fprintf('Using %d singular values. Relative error: %g %%\n', ii, relerr)
     plot(t, f, 'b-', t, rec, 'r-+')
+    plot_text = text(1.3, 1.5, ...
+        sprintf(['Singular values:       %d\n' ...
+                 'Least singular value:  %e\n' ...
+                 'Relative error:        %.3g %%'], ii, sng, relerr), ...
+        'FontSize', 12);
     axis([0 maxt -1 2])
     pause
     
